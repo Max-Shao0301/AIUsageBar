843bd712fd5686ad52876a97255277878d546758
Max Shao <patrick123487@gmail.com>
2026-03-30 09:48:49 +0800

    Initial Commit

---
+++ AIUsageBar/AIUsageBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIUsageBarApp.swift
+//  AIUsageBar
+//
+//  Created by Max Shao on 2026/3/30.
+//
+
+import SwiftUI
+
+@main
+struct AIUsageBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIUsageBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIUsageBar
+//
+//  Created by Max Shao on 2026/3/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
